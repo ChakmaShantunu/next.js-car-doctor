e8fb6214913dc1a290dab482b8128df6b8807a37
shantunu007 <chakmashantunu.web@gmail.com>
2025-10-23 18:04:43 +0600

    service details banner details display done

--- src/app/services/[id]/page.jsx
+++ src/app/services/[id]/page.jsx
@@ -1,11 +1,31 @@
+import dbConnect, { collectionNamesObj } from '@/lib/dbConnect'
+import { ObjectId } from 'mongodb';
+import Image from 'next/image';
 import React from 'react'
 
 export default async function ServiceDetailsPage({ params }) {
 
     const p = await params
+    const servicesCollection = dbConnect(collectionNamesObj.servicesCollection);
+    const data = await servicesCollection.findOne({ _id: new ObjectId(p.id) });
     return (
         <div>
-            <p>{p.id}</p>
+            <section>
+                <div className="flex justify-center w-full my-12">
+                    <div className="relative w-[1200px] h-[300px] overflow-hidden rounded-3xl">
+                        <Image
+                            src="/assets/images/checkout/checkout.png"
+                            alt="banner"
+                            fill
+                            style={{ objectFit: 'cover', objectPosition: 'center' }}
+                            
+                        />
+                        <div className="absolute inset-0 bg-gradient-to-r from-black/90 via-black/10 to-transparent"></div>
+                    </div>
+                </div>
+            </section>
+
+            <p>{JSON.stringify(data)}</p>
         </div>
     )
 }
