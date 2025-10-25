0fec983eb60dee9e24fafd3e26016964dac3bc7f
shantunu007 <chakmashantunu.web@gmail.com>
2025-10-25 21:27:55 +0600

    register form updated under register components

---
+++ src/app/register/components/RegisterForm.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function RegisterForm() {
+    return (
+        <div>
+            <form className="fieldset space-y-2 py-4">
+                <label className="label text-black">Name</label>
+                <input type="text" name='name' className="input" placeholder="Your Name" />
+                <label className="label text-black">Email</label>
+                <input type="email" name='email' className="input" placeholder="Email" />
+                {/* <label className="label text-black">Photo-URL</label>
+                        <input type="text" name='photo' className="input" placeholder="Photo-URL" /> */}
+                <label className="label text-black">Password</label>
+                <input type="password" name='password' className="input" placeholder="Password" />
+
+                <button className="btn btn-primary mt-4">Register</button>
+            </form>
+        </div>
+    )
+}
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
+import RegisterForm from './components/RegisterForm'
 
 export default function RegisterPage() {
     return (
@@ -8,18 +9,7 @@ export default function RegisterPage() {
                 <h3 className="text-2xl font-bold text-center pt-8 text-black">Create an Account!</h3>
                 <p className='text-xs text-center mt-2 text-black'>Join now to streamline your experience from day one</p>
                 <div className="card-body">
-                    <form className="fieldset space-y-2 py-4">
-                        <label className="label text-black">Name</label>
-                        <input type="text" name='name' className="input" placeholder="Your Name" />
-                        <label className="label text-black">Email</label>
-                        <input type="email" name='email' className="input" placeholder="Email" />
-                        {/* <label className="label text-black">Photo-URL</label>
-                        <input type="text" name='photo' className="input" placeholder="Photo-URL" /> */}
-                        <label className="label text-black">Password</label>
-                        <input type="password" name='password' className="input" placeholder="Password" />
-
-                        <button className="btn btn-primary mt-4">Register</button>
-                    </form>
+                    <RegisterForm></RegisterForm>
                     <p className='text-black'>Already have an account? Please <Link
                         className='underline' href={'/login'}>Login</Link></p>
                 </div>
